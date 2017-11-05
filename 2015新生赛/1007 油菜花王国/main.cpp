f367323f5333b18ab27c69a967cded74b7c71a0f
NightCats <1279707779@qq.com>
2017-11-05 17:31:59 +0800

    17.11.05

--- 2015新生赛/1007 油菜花王国/main.cpp
+++ 2015新生赛/1007 油菜花王国/main.cpp
@@ -6,6 +6,8 @@ vector<int>fib;
 int sprite[1005];
 int relation[5005];
 int par[5005];
+int fam[5005];
+int fam_k;
 int ans[5005];
 
 void getFib() {
@@ -25,17 +27,38 @@ int main()
 
   getFib();
 
-  int n, m, k;
+  int n, m, k, a, b;
   while (cin >> n >> m) {
+    //初始化变量
+    fam_k = 0;
     //读取精灵数据
     for (int i = 0; i < n; ++i) {
       cin >> k;
       if(binary_search(fib.begin(), fib.end(), k))
-        sprite[i] = 1;
+        sprite[i+1] = 1;
       else
-        sprite[i] = 0;
+        sprite[i+1] = 0;
     }
     //读取关系链
+    for (int j = 0; j < m; ++j) {
+      cin >> a >> b;
+      if (par[a] == par[b] && par[a] == 0) {
+        par[a] = a;
+        fam[a] = ++fam_k;
+        ans[fam[par[a]]] += sprite[a];
+      }
+      if (par[a] == 0 && par[b] != 0) {
+        par[a] = par[b];
+        ans[fam[par[b]]] += sprite[a];
+      }
+      if (par[b] == 0 && par[a] != 0) {
+        par[b] = par[a];
+        ans[fam[par[a]]] += sprite[b];
+      }
+    }
+    for (int i = 1; i <= fam_k; ++i) {
+      cout << ans[i] << endl;
+    }
   }
 
 
